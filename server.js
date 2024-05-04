9acf342b8d2b20d3f6488e644cde1debd6ebb24f
Vishaka Parab <112851204+vishaka1203@users.noreply.github.com>
2024-05-04 20:48:56 +0530

    backend for sending emails (#1)
    * backend for sending emails
    
    * backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const Server = express();
 const port = 8000;
@@ -11,8 +12,19 @@ Server.use(cors());
 // Middleware to parse JSON bodies
 Server.use(bodyParser.json());
 
+// Nodemailer transporter configuration
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'Vishaka7721@gmail.com',
+    pass: '', // TODO: GENERATE PASSWORD HERE https://security.google.com/settings/security/apppasswords
+  },
+});
+
 // Endpoint to handle appointment booking
-Server.post('/book-appointment', (req, res) => {
+Server.post('/book-appointment', async (req, res) => {
   const {
     selectedDate,
     selectedTime,
@@ -30,7 +42,34 @@ Server.post('/book-appointment', (req, res) => {
   console.log(`Mobile: ${mobile}`);
   console.log(`Problem Description: ${problemDescription}`);
 
-  res.status(200).send('Appointment booked successfully');
+  // Simulate booking appointment with timeout
+  setTimeout(() => {
+    res.status(200).send('Appointment booked successfully');
+  }, 1000); // Simulate delay before setting booked state
+});
+
+// Endpoint to send email to the selected doctor
+Server.post('/send-email', async (req, res) => {
+  console.log(req.body)
+  const { doctorEmail, formData } = req.body;
+
+  console.log(doctorEmail, formData)
+
+  const mailOptions = {
+    from: 'Vishaka7721@gmail.com',
+    to: doctorEmail,
+    subject: 'New Appointment',
+    text: `You have a new appointment request:\n\nDate: ${formData.selectedDate}\nTime: ${formData.selectedTime}\nName: ${formData.name}\nEmail: ${formData.email}\nMobile: ${formData.mobile}\nProblem Description: ${formData.problemDescription}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent to doctor successfully');
+    res.status(200).send('Email sent to doctor successfully');
+  } catch (error) {
+    console.error('Error sending email to doctor:', error);
+    res.status(500).send('Error sending email to doctor');
+  }
 });
 
 Server.listen(port, () => {
